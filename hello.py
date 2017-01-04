42bdb0dbb3da821cda6520c6a5d8b0e24e963c06
wangchaoyu <wangchaoyu@wangchaoyudeMacBook-Pro.local>
2017-01-04 22:59:45 +0800

    init

---
+++ hello.py
@@ -0,0 +1,177 @@
+import os
+from datetime import datetime
+from flask import Flask, render_template, session, redirect, url_for, flash, request, abort
+from flask.ext.login import login_manager,login_user
+from flask_login import login_user, logout_user, login_required, LoginManager, login_required, current_user
+from flask.ext.script import Manager, Shell
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+# from sqlalchemy.testing.pickleable import User
+from wtforms import StringField, SubmitField, ValidationError
+from wtforms.validators import Required, EqualTo
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import Form as BaseForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email
+from flask_login import UserMixin
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+dbpath = 'basedir' + 'data.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.String(64))
+    password = db.Column(db.String(128))
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class RegistrationForm(Form):
+    username = StringField('Username', validators=[
+        Required(), Length(1, 64)])
+    password = PasswordField('Password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    passworld = StringField('What is your password?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class LoginForm(Form):
+    username = StringField('username', validators=[Required(), Length(1, 64)])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+
+def password_hash(password):
+    str = generate_password_hash(password)
+    return str
+
+def verifytt_password(self,password):
+    return check_password_hash(self.password, password)
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data, role_id='2', password=password_hash(form.passworld.data))
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+            flag = verifytt_password(user, form.passworld.data)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    db.create_all()
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None and verifytt_password(user, form.password.data):
+            login_user(user, form.remember_me.data)
+            user.last_seen = datetime.utcnow()
+            db.session.add(user)
+            db.session.commit()
+            return redirect(request.args.get('next') or url_for("index"))
+        flash('Invalid username or password.')
+    return render_template('login.html', form=form)
+
+
+# @login_manager.user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,role_id='2',password=password_hash(form.password.data),member_since=datetime.utcnow())
+        db.session.add(user)
+        db.session.commit()
+        flash('welcome brother')
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    manager.run()
